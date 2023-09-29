c6129e9b69b47380c8c6970eff4d519540fdc288
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2023-09-29 10:30:57 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <h1>Code Couture</h1>
+      <h1>Code Counture</h1>
       <p>Tu código, tu estilo</p>
     </header>
   );
